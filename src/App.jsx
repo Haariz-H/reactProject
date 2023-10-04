d68bae1f321cd2ccabce359968e3b5642bd27257
Haariz-H <hasanhaariz619@gmail.com>
2023-10-04 15:38:01 +0530

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  let username = "Kaneki";
+  let isLoggedIn = "False";
+  // if(isLoggedIn){
+
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>Welcome {isLoggedIn === "True" ? username : "Anonymous User"}</div>
   );
 }
 
 export default App;
+{
+  /* <div
+id="carouselExampleInterval"
+class="carousel slide"
+data-bs-ride="carousel"
+>
+<div class="carousel-inner">
+  <div class="carousel-item active" data-bs-interval="2">
+    <img src="l.jpg" class="d-block w-100" alt="..." />
+  </div>
+  <div class="carousel-item" data-bs-interval="2">
+    <img src="light.jpg" class="d-block w-100" alt="..." />
+  </div>
+  <div class="carousel-item" data-bs-interval="2">
+    <img src="yagami.jpg" class="d-block w-100" alt="..." />
+  </div>
+</div>
+<button
+  class="carousel-control-prev"
+  type="button"
+  data-bs-target="#carouselExampleInterval"
+  data-bs-slide="prev"
+>
+  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+  <span class="visually-hidden">Previous</span>
+</button>
+<button
+  class="carousel-control-next"
+  type="button"
+  data-bs-target="#carouselExampleInterval"
+  data-bs-slide="next"
+>
+  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+  <span class="visually-hidden">Next</span>
+</button>
+</div> */
+}
