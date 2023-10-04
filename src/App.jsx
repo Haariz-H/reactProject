ddfffbc35a0642b2cc0c784f76cf57a8a6876595
Haariz-H <hasanhaariz619@gmail.com>
2023-10-04 18:42:27 +0530

    Image Changed

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,41 @@ function App() {
 
   return (
     <div>
+      <div
+        id="carouselExampleInterval"
+        class="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div class="carousel-inner">
+          <div class="carousel-item active" data-bs-interval="2">
+            <img src="johan.jpg" class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item" data-bs-interval="2">
+            <img src="tenma.jpg" class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item" data-bs-interval="2">
+            <img src="dan.jpg" class="d-block w-100" alt="..." />
+          </div>
+        </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleInterval"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleInterval"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
       <ul>
         {characters.map((character) => (
           <li>{character}</li>
@@ -15,40 +50,3 @@ function App() {
 }
 
 export default App;
-{
-  /* <div
-id="carouselExampleInterval"
-class="carousel slide"
-data-bs-ride="carousel"
->
-<div class="carousel-inner">
-  <div class="carousel-item active" data-bs-interval="2">
-    <img src="l.jpg" class="d-block w-100" alt="..." />
-  </div>
-  <div class="carousel-item" data-bs-interval="2">
-    <img src="light.jpg" class="d-block w-100" alt="..." />
-  </div>
-  <div class="carousel-item" data-bs-interval="2">
-    <img src="yagami.jpg" class="d-block w-100" alt="..." />
-  </div>
-</div>
-<button
-  class="carousel-control-prev"
-  type="button"
-  data-bs-target="#carouselExampleInterval"
-  data-bs-slide="prev"
->
-  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-  <span class="visually-hidden">Previous</span>
-</button>
-<button
-  class="carousel-control-next"
-  type="button"
-  data-bs-target="#carouselExampleInterval"
-  data-bs-slide="next"
->
-  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-  <span class="visually-hidden">Next</span>
-</button>
-</div> */
-}
--- src/index.js
+++ src/index.js
@@ -7,12 +7,10 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render( <
-    React.StrictMode >
-    <
-    App / >
-    <
-    /React.StrictMode>
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
